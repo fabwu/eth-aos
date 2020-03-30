618520c77968cb5e50c92e7a9a78230b8bc9037f
Janis Peyer <jpeyer@student.ethz.ch>
2020-03-30 19:28:34 +0200

    Implemented byte array and string in lmp_protocol

--- include/aos/lmp_protocol.h
+++ include/aos/lmp_protocol.h
@@ -6,7 +6,7 @@
 #ifndef _LIB_BARRELFISH_LMP_PROTOCOL_H
 #define _LIB_BARRELFISH_LMP_PROTOCOL_H
 
-errval_t lmp_protocol_send(struct lmp_chan *chan, uint8_t message_type, struct capref cap,
+errval_t lmp_protocol_send(struct lmp_chan *chan, uint16_t message_type, struct capref cap,
                            uintptr_t arg1, uintptr_t arg2, uintptr_t arg3);
 
 #define lmp_protocol_send0(chan, msg_type)                                               \
@@ -26,7 +26,7 @@ errval_t lmp_protocol_send(struct lmp_chan *chan, uint8_t message_type, struct c
 #define lmp_protocol_send_cap3(chan, msg_type, send_cap, arg1, arg2, arg3)               \
     lmp_protocol_send((chan), (msg_type), (send_cap), (arg1), (arg2), (arg3))
 
-errval_t lmp_protocol_recv(struct lmp_chan *chan, uint8_t message_type,
+errval_t lmp_protocol_recv(struct lmp_chan *chan, uint16_t message_type,
                            struct capref *ret_cap, uintptr_t *ret_arg1,
                            uintptr_t *ret_arg2, uintptr_t *ret_arg3);
 
@@ -47,24 +47,24 @@ errval_t lmp_protocol_recv(struct lmp_chan *chan, uint8_t message_type,
 #define lmp_protocol_recv_cap3(chan, msg_type, ret_cap, ret_arg1, ret_arg2, ret_arg3)    \
     lmp_protocol_recv((chan), (msg_type), (ret_cap), (ret_arg1), (ret_arg2), (ret_arg3))
 
-errval_t lmp_protocol_send_bytes_cap(struct lmp_chan *chan, uint8_t message_type,
-                                     struct capref cap, size_t size, int8_t *bytes);
-errval_t lmp_protocol_recv_bytes_cap(struct lmp_chan *chan, uint8_t message_type,
+errval_t lmp_protocol_send_bytes_cap(struct lmp_chan *chan, uint16_t message_type,
+                                     struct capref cap, size_t size, const uint8_t *bytes);
+errval_t lmp_protocol_recv_bytes_cap(struct lmp_chan *chan, uint16_t message_type,
                                      struct capref *ret_cap, size_t *ret_size,
-                                     int8_t **ret_bytes);
+                                     uint8_t **ret_bytes);
 #define lmp_protocol_send_bytes(chan, msg_type, size, bytes)                             \
     lmp_protocol_send_bytes_cap((chan), (msg_type), NULL_CAP, (size), (bytes))
 #define lmp_protocol_recv_bytes(chan, msg_type, ret_size, ret_bytes)                     \
     lmp_protocol_recv_bytes_cap((chan), (msg_type), NULL, (ret_size), (ret_bytes))
 
-errval_t lmp_protocol_send_string_cap(struct lmp_chan *chan, uint8_t message_type,
-                                      struct capref cap, char *string);
-errval_t lmp_protocol_recv_string_cap(struct lmp_chan *chan, uint8_t message_type,
-                                      struct capref *ret_cap, char *string);
+errval_t lmp_protocol_send_string_cap(struct lmp_chan *chan, uint16_t message_type,
+                                      struct capref cap, const char *string);
+errval_t lmp_protocol_recv_string_cap(struct lmp_chan *chan, uint16_t message_type,
+                                      struct capref *ret_cap, char **ret_string);
 #define lmp_protocol_send_string(chan, msg_type, string)                                 \
-    lmp_protocol_send_string((chan), (msg_type), NULL_CAP, (string))
+    lmp_protocol_send_string_cap((chan), (msg_type), NULL_CAP, (string))
 #define lmp_protocol_recv_string(chan, msg_type, ret_string)                             \
-    lmp_protocol_send_string((chan), (msg_type), NULL, (ret_string))
+    lmp_protocol_recv_string_cap((chan), (msg_type), NULL, (ret_string))
 
 
 #endif
--- lib/aos/lmp_protocol.c
+++ lib/aos/lmp_protocol.c
@@ -6,18 +6,23 @@
 #include <aos/aos.h>
 #include <aos/lmp_protocol.h>
 
+#define LMP_PROTOCOL_DATA_LENGTH 3
+#define LMP_PROTOCOL_DATA_ENTRY_SIZE sizeof(uintptr_t)
+#define LMP_PROTOCOL_DATA_BYTES (LMP_PROTOCOL_DATA_LENGTH * LMP_PROTOCOL_DATA_ENTRY_SIZE)
+
 struct lmp_msg_state {
     struct lmp_chan *chan;
-    uint8_t message_type;
+    uint16_t message_type;
     struct capref cap;
     uintptr_t data[3];
     bool done;
     bool failed;
 };
 
-static struct lmp_msg_state make_lmp_msg_state(struct lmp_chan *chan, uint8_t message_type,
-                                               struct capref cap, uintptr_t arg1,
-                                               uintptr_t arg2, uintptr_t arg3)
+static struct lmp_msg_state make_lmp_msg_state(struct lmp_chan *chan,
+                                               uint16_t message_type, struct capref cap,
+                                               uintptr_t arg1, uintptr_t arg2,
+                                               uintptr_t arg3)
 {
     struct lmp_msg_state state;
     state.chan = chan;
@@ -25,8 +30,6 @@ static struct lmp_msg_state make_lmp_msg_state(struct lmp_chan *chan, uint8_t me
     state.data[0] = arg1;
     state.data[1] = arg2;
     state.data[2] = arg3;
-    state.done = false;
-    state.failed = false;
     return state;
 }
 
@@ -54,8 +57,9 @@ static void lmp_protocol_send_closure(void *arg)
     errval_t err;
 
     struct lmp_msg_state *state = (struct lmp_msg_state *)arg;
-    err = lmp_chan_send4(state->chan, LMP_SEND_FLAGS_DEFAULT, state->cap, state->message_type,
-                         state->data[0], state->data[1], state->data[2]);
+    err = lmp_chan_send4(state->chan, LMP_SEND_FLAGS_DEFAULT, state->cap,
+                         state->message_type, state->data[0], state->data[1],
+                         state->data[2]);
 
     if (err_is_ok(err)) {
         state->done = true;
@@ -73,65 +77,16 @@ static void lmp_protocol_send_closure(void *arg)
     DEBUG_ERR(err, "send_closure failed hard");
 }
 
-/**
- * \brief Receive lmp message with given message type using the given lmp_msg_state.
- */
-static void lmp_protocol_recv_closure(void *arg)
+static errval_t lmp_protocol_send_state(struct lmp_msg_state *state)
 {
-    errval_t err;
+    state->done = false;
+    state->failed = false;
 
-    struct lmp_msg_state *st = (struct lmp_msg_state *)arg;
-    struct lmp_recv_msg msg = LMP_RECV_MSG_INIT;
-
-    err = lmp_chan_recv(st->chan, &msg, &st->cap);
-
-    if (err_is_ok(err)) {
-        // Check message type
-        assert(msg.words[0] == st->message_type);
-        if (msg.words[0] != st->message_type) {
-            st->failed = true;
-        }
-
-        if (!capref_is_null(st->cap)) {
-            // Allocate slot for next receive
-            err = lmp_chan_alloc_recv_slot(st->chan);
-            if (err_is_fail(err)) {
-                DEBUG_ERR(err, "Couldn't allocate slot for ret_cap");
-            }
-        }
-
-        st->data[0] = msg.words[1];
-        st->data[1] = msg.words[2];
-        st->data[2] = msg.words[3];
-        st->done = true;
-        return;
-    } else if (lmp_err_is_transient(err)) {
-        err = lmp_chan_register_recv(st->chan, get_default_waitset(),
-                                     MKCLOSURE(lmp_protocol_recv_closure, arg));
-        if (err_is_ok(err)) {
-            return;
-        }
-    }
-
-    st->done = true;
-    st->failed = true;
-    DEBUG_ERR(err, "recv_closure failed hard");
-}
-
-/**
- * \brief Send given lmp message over given lmp channel.
- */
-errval_t lmp_protocol_send(struct lmp_chan *chan, uint8_t message_type, struct capref cap,
-                           uintptr_t arg1, uintptr_t arg2, uintptr_t arg3)
-{
-    struct lmp_msg_state state = make_lmp_msg_state(chan, message_type, cap, arg1, arg2,
-                                                    arg3);
-
-    lmp_protocol_send_closure(&state);
-    errval_t err = lmp_protocol_wait_for(&state.done);
+    lmp_protocol_send_closure(state);
+    errval_t err = lmp_protocol_wait_for(&state->done);
     if (err_is_fail(err)) {
         return err_push(err, AOS_ERR_LMP_SEND_FAILURE);
-    } else if (state.failed) {
+    } else if (state->failed) {
         return AOS_ERR_LMP_SEND_FAILURE;
     }
 
@@ -139,30 +94,100 @@ errval_t lmp_protocol_send(struct lmp_chan *chan, uint8_t message_type, struct c
 }
 
 /**
- * \brief Receive lmp message of the given message type using given lmp channel.
+ * \brief Receive lmp message with given message type using the given lmp_msg_state.
  */
-errval_t lmp_protocol_recv(struct lmp_chan *chan, uint8_t message_type,
-                           struct capref *ret_cap, uintptr_t *ret_arg1,
-                           uintptr_t *ret_arg2, uintptr_t *ret_arg3)
+static void lmp_protocol_recv_closure(void *arg)
 {
     errval_t err;
 
+    struct lmp_msg_state *state = (struct lmp_msg_state *)arg;
+    struct lmp_recv_msg msg = LMP_RECV_MSG_INIT;
+
+    err = lmp_chan_recv(state->chan, &msg, &state->cap);
+
+    if (err_is_ok(err)) {
+        // Check message type
+        assert(msg.words[0] == state->message_type);
+        if (msg.words[0] != state->message_type) {
+            state->failed = true;
+        }
+
+        if (!capref_is_null(state->cap)) {
+            // Allocate slot for next receive
+            err = lmp_chan_alloc_recv_slot(state->chan);
+            if (err_is_fail(err)) {
+                DEBUG_ERR(err, "Couldn't allocate slot for ret_cap");
+            }
+        }
+
+        state->data[0] = msg.words[1];
+        state->data[1] = msg.words[2];
+        state->data[2] = msg.words[3];
+        state->done = true;
+        return;
+    } else if (lmp_err_is_transient(err)) {
+        err = lmp_chan_register_recv(state->chan, get_default_waitset(),
+                                     MKCLOSURE(lmp_protocol_recv_closure, arg));
+        if (err_is_ok(err)) {
+            return;
+        }
+    }
+
+    state->done = true;
+    state->failed = true;
+    DEBUG_ERR(err, "recv_closure failed hard");
+}
+
+static errval_t lmp_protocol_recv_state(struct lmp_msg_state *state)
+{
+    errval_t err;
+
+    state->done = false;
+    state->failed = false;
+
+    err = lmp_chan_register_recv(state->chan, get_default_waitset(),
+                                 MKCLOSURE(lmp_protocol_recv_closure, state));
+    if (err_is_fail(err)) {
+        return err_push(err, AOS_ERR_LMP_RECV_FAILURE);
+    }
+
+    err = lmp_protocol_wait_for(&state->done);
+    if (err_is_fail(err)) {
+        return err_push(err, AOS_ERR_LMP_RECV_FAILURE);
+    } else if (state->failed) {
+        return AOS_ERR_LMP_RECV_FAILURE;
+    }
+
+    return SYS_ERR_OK;
+}
+
+/**
+ * \brief Send given lmp message over given lmp channel.
+ */
+errval_t lmp_protocol_send(struct lmp_chan *chan, uint16_t message_type, struct capref cap,
+                           uintptr_t arg1, uintptr_t arg2, uintptr_t arg3)
+{
+    assert((message_type & 0xff00) && (message_type & 0xff));
+    struct lmp_msg_state state = make_lmp_msg_state(chan, message_type, cap, arg1, arg2,
+                                                    arg3);
+    return lmp_protocol_send_state(&state);
+}
+
+/**
+ * \brief Receive lmp message of the given message type using given lmp channel.
+ */
+errval_t lmp_protocol_recv(struct lmp_chan *chan, uint16_t message_type,
+                           struct capref *ret_cap, uintptr_t *ret_arg1,
+                           uintptr_t *ret_arg2, uintptr_t *ret_arg3)
+{
+    assert((message_type & 0xff00) && (message_type & 0xff));
     struct lmp_msg_state state;
     state.chan = chan;
     state.message_type = message_type;
-    state.failed = false;
-    state.done = false;
 
-    err = lmp_chan_register_recv(state.chan, get_default_waitset(),
-                                 MKCLOSURE(lmp_protocol_send_closure, &state));
-    if(err_is_fail(err)) {
-        return err_push(err, AOS_ERR_LMP_RECV_FAILURE);
-    }
-    err = lmp_protocol_wait_for(&state.done);
+    errval_t err = lmp_protocol_recv_state(&state);
     if (err_is_fail(err)) {
-        return err_push(err, AOS_ERR_LMP_RECV_FAILURE);
-    } else if (state.failed) {
-        return AOS_ERR_LMP_RECV_FAILURE;
+        return err;
     }
 
     if (ret_cap != NULL) {
@@ -181,22 +206,120 @@ errval_t lmp_protocol_recv(struct lmp_chan *chan, uint8_t message_type,
     return SYS_ERR_OK;
 }
 
-errval_t lmp_protocol_send_bytes_cap(struct lmp_chan *chan, uint8_t message_type,
-                                     struct capref cap, size_t size, int8_t *bytes)
+/**
+ * \brief Send given bytes over given lmp channel.
+ *
+ * The first message contains the amount of bytes and the first 2*8 bytes. All later
+ * messages contain <= 3*8 bytes until all bytes have been sent. The last message contains
+ * the remaining bytes followed by 0 entries.
+ */
+errval_t lmp_protocol_send_bytes_cap(struct lmp_chan *chan, uint16_t message_type,
+                                     struct capref cap, size_t size, const uint8_t *bytes)
 {
+    errval_t err;
+
+    assert((message_type & 0xff00) && (message_type & 0xff));
+    struct lmp_msg_state state = make_lmp_msg_state(chan, message_type, cap, size, 0, 0);
+
+    memcpy(&state.data[1], bytes, MIN(size, 2 * LMP_PROTOCOL_DATA_ENTRY_SIZE));
+    size_t offset = 2 * LMP_PROTOCOL_DATA_ENTRY_SIZE;
+
+    err = lmp_protocol_send_state(&state);
+    if (err_is_fail(err)) {
+        return err;
+    }
+
+    state.cap = NULL_CAP;
+    while (offset < size) {
+        size_t send_size = LMP_PROTOCOL_DATA_BYTES;
+        if (size - offset < LMP_PROTOCOL_DATA_BYTES) {
+            send_size = size - offset;
+            memset(state.data + send_size, 0, LMP_PROTOCOL_DATA_BYTES - send_size);
+        }
+        memcpy(&state.data, bytes + offset, send_size);
+        offset += send_size;
+
+        err = lmp_protocol_send_state(&state);
+        if (err_is_fail(err)) {
+            return err;
+        }
+    }
+
+    assert(offset == size);
+    return SYS_ERR_OK;
 }
 
-errval_t lmp_protocol_recv_bytes_cap(struct lmp_chan *chan, uint8_t message_type,
+/**
+ * \brief Receive bytes over the given lmp channel.
+ *
+ * It designed to receive messages sent by lmp_protocol_send_bytes_cap() and follows the
+ * protocol described there. The returned array is allocated using malloc and has to be
+ * freed by the client after it is no longer used.
+ */
+errval_t lmp_protocol_recv_bytes_cap(struct lmp_chan *chan, uint16_t message_type,
                                      struct capref *ret_cap, size_t *ret_size,
-                                     int8_t **ret_bytes)
+                                     uint8_t **ret_bytes)
 {
+    assert((message_type & 0xff00) && (message_type & 0xff));
+    assert(ret_bytes != NULL);
+    struct lmp_msg_state state;
+    state.chan = chan;
+    state.message_type = message_type;
+
+    errval_t err = lmp_protocol_recv_state(&state);
+    if (err_is_fail(err)) {
+        return err;
+    }
+
+    if (ret_cap != NULL) {
+        *ret_cap = state.cap;
+    }
+
+    size_t size = state.data[0];
+    if (ret_size != NULL) {
+        *ret_size = size;
+    }
+
+    *ret_bytes = (uint8_t *)malloc(size);
+
+    memcpy(*ret_bytes, &state.data[1], MIN(size, 2 * LMP_PROTOCOL_DATA_ENTRY_SIZE));
+    size_t offset = 2 * LMP_PROTOCOL_DATA_ENTRY_SIZE;
+
+    while (offset < size) {
+        err = lmp_protocol_recv_state(&state);
+        if (err_is_fail(err)) {
+            return err;
+        }
+
+        size_t recv_size = MIN(size - offset, LMP_PROTOCOL_DATA_BYTES);
+        memcpy(*ret_bytes + offset, state.data, recv_size);
+        offset += recv_size;
+    }
+
+    assert(offset == size);
+    return SYS_ERR_OK;
 }
 
-errval_t lmp_protocol_send_string_cap(struct lmp_chan *chan, uint8_t message_type,
-                                      struct capref cap, char *string)
+/**
+ * \brief Sends the given \0 terminated string over the given lmp channel.
+ */
+errval_t lmp_protocol_send_string_cap(struct lmp_chan *chan, uint16_t message_type,
+                                      struct capref cap, const char *string)
 {
+    size_t size = strlen(string) + 1;
+    return lmp_protocol_send_bytes_cap(chan, message_type, cap, size,
+                                       (const uint8_t *)string);
 }
-errval_t lmp_protocol_recv_string_cap(struct lmp_chan *chan, uint8_t message_type,
-                                      struct capref *ret_cap, char *string)
+
+/**
+ * \brief Receives the given \0 terminated string from the given lmp channel.
+ *
+ * The returned string is allocated using malloc and has to be freed by the client after
+ * it is no longer used.
+ */
+errval_t lmp_protocol_recv_string_cap(struct lmp_chan *chan, uint16_t message_type,
+                                      struct capref *ret_cap, char **ret_string)
 {
+    return lmp_protocol_recv_bytes_cap(chan, message_type, ret_cap, NULL,
+                                       (uint8_t **)ret_string);
 }
