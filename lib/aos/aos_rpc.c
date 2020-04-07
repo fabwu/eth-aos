8b0e409b53f388c29406c17de81e72d243724931
Janis Peyer <jpeyer@student.ethz.ch>
2020-04-07 10:45:31 +0200

    Added additional asserts to aos_rpc.c

--- lib/aos/aos_rpc.c
+++ lib/aos/aos_rpc.c
@@ -19,6 +19,14 @@
 /// RPC channel to init
 static struct aos_rpc rpc_init;
 
+static void aos_rpc_assert(struct aos_rpc *rpc)
+{
+    assert(rpc != NULL);
+    assert(rpc->chan.endpoint != NULL);
+    assert(!capref_is_null(rpc->chan.remote_cap));
+    assert(!capref_is_null(rpc->chan.local_cap));
+}
+
 void aos_rpc_set_init_channel(struct lmp_chan chan)
 {
     rpc_init.chan = chan;
@@ -26,16 +34,19 @@ void aos_rpc_set_init_channel(struct lmp_chan chan)
 
 errval_t aos_rpc_send_number(struct aos_rpc *rpc, uintptr_t num)
 {
+    aos_rpc_assert(rpc);
     return lmp_protocol_send1(&rpc->chan, AOS_RPC_SEND_NUMBER, num);
 }
 
 errval_t aos_rpc_send_string(struct aos_rpc *rpc, const char *string)
 {
+    aos_rpc_assert(rpc);
     return lmp_protocol_send_string(&rpc->chan, AOS_RPC_SEND_STRING, string);
 }
 
 errval_t aos_rpc_recv_string(struct aos_rpc *rpc, char **string)
 {
+    aos_rpc_assert(rpc);
     return lmp_protocol_recv_string(&rpc->chan, AOS_RPC_SEND_STRING, string);
 }
 
@@ -43,6 +54,7 @@ errval_t aos_rpc_get_ram_cap(struct aos_rpc *rpc, size_t bytes, size_t alignment
                              struct capref *ret_cap, size_t *ret_bytes)
 {
     errval_t err;
+    aos_rpc_assert(rpc);
 
     // Request ram cap
     err = lmp_protocol_send2(&rpc->chan, AOS_RPC_GET_RAM_CAP, bytes, alignment);
@@ -77,6 +89,7 @@ errval_t aos_rpc_get_ram_cap(struct aos_rpc *rpc, size_t bytes, size_t alignment
 errval_t aos_rpc_free_ram_cap(struct aos_rpc *rpc, genpaddr_t addr)
 {
     errval_t err;
+    aos_rpc_assert(rpc);
 
     // Try freeing RAM
     err = lmp_protocol_send1(&rpc->chan, AOS_RPC_FREE_RAM_CAP, addr);
@@ -104,11 +117,13 @@ errval_t aos_rpc_free_ram_cap(struct aos_rpc *rpc, genpaddr_t addr)
 
 errval_t aos_rpc_serial_getchar(struct aos_rpc *rpc, char *retc)
 {
+    aos_rpc_assert(rpc);
     return lmp_protocol_send0(&rpc->chan, AOS_RPC_SERIAL_GETCHAR);
 }
 
 errval_t aos_rpc_serial_putchar(struct aos_rpc *rpc, char c)
 {
+    aos_rpc_assert(rpc);
     return lmp_protocol_send1(&rpc->chan, AOS_RPC_SERIAL_PUTCHAR, c);
 }
 
@@ -116,6 +131,7 @@ errval_t aos_rpc_process_spawn(struct aos_rpc *rpc, char *cmdline, coreid_t core
                                domainid_t *newpid)
 {
     errval_t err;
+    aos_rpc_assert(rpc);
     uintptr_t ret_pid = 0;
     uintptr_t ret_success = 0;
 
@@ -151,6 +167,7 @@ errval_t aos_rpc_process_spawn(struct aos_rpc *rpc, char *cmdline, coreid_t core
 
 errval_t aos_rpc_process_get_name(struct aos_rpc *rpc, domainid_t pid, char **name)
 {
+    aos_rpc_assert(rpc);
     // TODO (M5): implement name lookup for process given a process id
     return SYS_ERR_OK;
 }
@@ -159,7 +176,8 @@ errval_t aos_rpc_process_get_name(struct aos_rpc *rpc, domainid_t pid, char **na
 errval_t aos_rpc_process_get_all_pids(struct aos_rpc *rpc, domainid_t **pids,
                                       size_t *pid_count)
 {
-    // TODO (M5): implement process id discovery
+    // errval_t err;
+    aos_rpc_assert(rpc);
     return SYS_ERR_OK;
 }
 
@@ -167,6 +185,7 @@ errval_t aos_rpc_process_get_all_pids(struct aos_rpc *rpc, domainid_t **pids,
 errval_t aos_rpc_get_device_cap(struct aos_rpc *rpc, lpaddr_t paddr, size_t bytes,
                                 struct capref *ret_cap)
 {
+    aos_rpc_assert(rpc);
     return LIB_ERR_NOT_IMPLEMENTED;
 }
 
