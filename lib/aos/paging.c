317e55ab03155e2ec88b1f21fc1c8a30bb218071
Fabian Wüthrich <me@fabwu.ch>
2020-05-07 07:55:20 +0200

    Silence some debug statements

--- lib/aos/paging.c
+++ lib/aos/paging.c
@@ -182,7 +182,7 @@ out:
 static errval_t handle_pagefault(lvaddr_t addr)
 {
     DEBUG_PAGING("handle_pagefault begin\n");
-    DEBUG_PRINTF("PAGEFAULT at 0x%x\n", addr);
+    DEBUG_PAGING("PAGEFAULT at %p\n", addr);
     errval_t err;
 
     // handle null pointer
